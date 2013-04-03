93f6a82552925c8fe8d0280b88f3a7f48de089b3
Christer Edvartsen <cogo@starzinger.net>
2013-04-03 12:44:25 +0200

    Added a Module.php in the root dir for compatibility with Zend Framework 2's module manager

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Imbo module for ZF2 package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+/**
+ * This file is placed here for compatibility with ZendFramework 2's ModuleManager. It allows usage
+ * of this module even without composer.
+ */
+require_once __DIR__ . '/src/Imbo/Module.php';
